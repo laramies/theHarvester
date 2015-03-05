7e131a0b1de719e9335af4526005d1a9962369d4
Peter McAlpine <peter.mcalpine@arcticwolf.com>
2015-03-05 14:04:50 -0500

    Fix EXTRAword.com matching when word is 'word.com'
    This change makes myparser.emails(...) stricter so that, given a word
    (eg: 'word.com') it does not match domains which end with that word
    (i.e. some-other-word.com). Subdomains continue to be matched (eg:
    'subdomain.word.com').
    
    Also add unit test for emails(...)

---
+++ myparser_test.py
@@ -0,0 +1,18 @@
+#
+# Unit tests for myparser.py
+#
+import myparser
+
+import unittest
+
+class TestMyParser(unittest.TestCase):
+
+  def test_emails(self):
+    word = 'domain.com'
+    results = '***a@domain***banotherdomain.com***c@domain.com***d@sub.domain.com***'
+    p = myparser.parser(results, word)
+    emails = sorted(p.emails())
+    self.assertEquals(emails, [ 'c@domain.com', 'd@sub.domain.com' ])
+
+if __name__ == '__main__':
+  unittest.main()
