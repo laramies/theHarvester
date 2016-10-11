a5796379fa71150c430fedc8506eef2184cddf91
laramies <laramies@gmail.com>
2016-10-11 09:27:38 +0100

    Fixed PGP keyserver search
    Changed the server to a different one. Still will be good to add a
    couple more and rotate them when one fails.

--- discovery/pgpsearch.py
+++ discovery/pgpsearch.py
@@ -9,10 +9,11 @@ class search_pgp:
     def __init__(self, word):
         self.word = word
         self.results = ""
-        self.server = "pgp.rediris.es:11371"
-        self.hostname = "pgp.rediris.es"
+        self.server = "pgp.mit.edu"
+        #self.server = "pgp.rediris.es:11371" Not  working at the moment
+        self.hostname = "pgp.mit.edu"
         self.userAgent = "(Mozilla/5.0 (Windows; U; Windows NT 6.0;en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
-
+        
     def process(self):
         h = httplib.HTTP(self.server)
         h.putrequest('GET', "/pks/lookup?search=" + self.word + "&op=index")
@@ -20,6 +21,8 @@ class search_pgp:
         h.putheader('User-agent', self.userAgent)
         h.endheaders()
         returncode, returnmsg, headers = h.getreply()
+        print returncode
+        print returnmsg
         self.results = h.getfile().read()
 
     def get_emails(self):
