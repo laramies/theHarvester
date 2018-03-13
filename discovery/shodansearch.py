3990d4ce9b89bd079608480faed263e7cd0271ef
Julian <sarlacii@users.noreply.github.com>
2018-03-13 16:14:17 +0200

    Update shodansearch.py
    Changed to use built-in shodan module, and not the custom API that was previously used. This brings operation in line with current shodan API.

--- discovery/shodansearch.py
+++ discovery/shodansearch.py
@@ -1,21 +1,21 @@
-from shodan import WebAPI
+# Do 'sudo pip2 install shodan' to get the built-in module
+import shodan
 import sys
 
+API_KEY = "oCiMsgM6rQWqiTvPxFHYcExlZgg7wvTt"
 
 class search_shodan():
 
     def __init__(self, host):
         self.host = host
-        self.key = "oCiMsgM6rQWqiTvPxFHYcExlZgg7wvTt"
-        if self.key == "":
-            print "You need an API key in order to use SHODAN database. You can get one here: http://www.shodanhq.com/"
-            sys.exit()
-        self.api = WebAPI(self.key)
+        self.api = shodan.Shodan(API_KEY)
 
     def run(self):
         try:
-            host = self.api.host(self.host)
-            return host['data']
+            result = self.api.search(self.host)
+            for service in result['matches']:
+                print ("%s:%s" % (service['ip_str'], service['port']))
+            return result
         except:
             print "SHODAN empty reply or error in the call"
             return "error"
