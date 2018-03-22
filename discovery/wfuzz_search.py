58b3853b1dbc9d57b260b532082af6c36ee96e5c
Christian Martorella <laramies@Chriss-MacBook-Pro.local>
2018-03-22 23:50:30 +0100

    Wfuzz plugin
    WIP to include wfuzz directory brute forcing

---
+++ discovery/wfuzz_search.py
@@ -0,0 +1,35 @@
+import string
+import requests
+import sys
+import myparser
+import re
+try:
+    import wfuzz
+except Exception, e: 
+    print e 
+
+class search_wfuzz:
+    def __init__(self, host):
+        self.host = host
+        self.results = ""
+        self.totalresults = ""
+       
+    def do_search(self):
+        print "elo"
+        try:
+            for r in wfuzz.fuzz(url="https://"+self.host+"/FUZZ", hc=[404], payloads=[("file",dict(fn="wordlist/general/common.txt"))]):
+                print r
+                self.results += r
+        except Exception, e:
+                print e
+        self.totalresults += self.results
+
+    def get_results(self):
+        return self.totalresults
+    
+    def do_check(self):
+        return
+
+    def process(self):
+        self.do_search()
+        print "\tSearching Wfuzz.."
