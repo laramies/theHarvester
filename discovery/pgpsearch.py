95e32aa3439bd6b0766e851a3e3c85d092752f3e
Christian Martorella <laramies@Chriss-MacBook-Pro.local>
2018-04-16 09:32:10 +0200

    3.0
    Added port_scanning and domain Takeover checks

--- discovery/pgpsearch.py
+++ discovery/pgpsearch.py
@@ -9,12 +9,13 @@ class search_pgp:
     def __init__(self, word):
         self.word = word
         self.results = ""
-        self.server = "pgp.mit.edu"
-        #self.server = "pgp.rediris.es:11371" Not  working at the moment
-        self.hostname = "pgp.mit.edu"
+        #self.server = "pgp.mit.edu"
+        self.server = "pgp.rediris.es"
+        self.hostname = "pgp.rediris.es"
         self.userAgent = "(Mozilla/5.0 (Windows; U; Windows NT 6.0;en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
         
     def process(self):
+        print "\tSearching PGP results..."
         h = httplib.HTTP(self.server)
         h.putrequest('GET', "/pks/lookup?search=" + self.word + "&op=index")
         h.putheader('Host', self.hostname)
