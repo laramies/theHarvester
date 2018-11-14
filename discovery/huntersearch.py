d26ce5573324ac935ae29a86bef28a3368b8fefd
Laramies <laramies@gmail.com>
2018-11-15 00:05:19 +0100

    Update discovery/huntersearch.py

--- discovery/huntersearch.py
+++ discovery/huntersearch.py
@@ -16,7 +16,7 @@ class search_hunter:
         self.results = ""
         self.totalresults = ""
         self.counter = start
-        self.database = "https://api.hunter.io/v2/domain-search?domain=" + word + "&api_key=" + self.key +"&limit="+self.limit
+        self.database = "https://api.hunter.io/v2/domain-search?domain=" + word + "&api_key=" + self.key +"&limit=" + str(self.limit)
 
     def do_search(self):
         try:
