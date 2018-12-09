fef88abc1d845d2194f7c42d1ab8539428ce36f1
jzold <janos.zold@hotmail.com>
2018-12-09 17:51:48 +0000

    adding cymon

--- theHarvester.py
+++ theHarvester.py
@@ -133,7 +133,7 @@ def start(argv):
             dnstld = True
         elif opt == '-b':
             engines = set(arg.split(','))
-            supportedengines = set(["baidu","bing","crtsh","censys","bingapi","dogpile","google","googleCSE","virustotal","threatcrowd","googleplus","google-profiles",'google-certificates',"linkedin","pgp","twitter","trello","vhost","yahoo","netcraft","hunter","all"])
+            supportedengines = set(["baidu","bing","crtsh","censys","cymon","bingapi","dogpile","google","googleCSE","virustotal","threatcrowd","googleplus","google-profiles",'google-certificates',"linkedin","pgp","twitter","trello","vhost","yahoo","netcraft","hunter","all"])
             if set(engines).issubset(supportedengines):
                 print("found supported engines")
                 print(("[-] Starting harvesting process for domain: " + word +  "\n"))
@@ -317,7 +317,17 @@ def start(argv):
                         db=stash.stash_manager()
                         db.store_all(word,all_hosts,'host','censys')
                         db.store_all(word,all_ip,'ip','censys')
-
+                        
+                    elif engineitem == "cymon":
+                        print("[-] Searching in Cymon:")
+                        from discovery import cymon
+                        #import locally or won't work
+                        search = cymon.search_cymon(word)
+                        search.process()
+                        all_emails = []
+                        all_ip = search.get_ipaddresses()
+                        db = stash.stash_manager()
+                        db.store_all(word,all_ip,'ip','cymon')
                     
                     elif engineitem == "trello":
                         print("[-] Searching in Trello:")
@@ -426,17 +436,31 @@ def start(argv):
                         search.process()
                         domains = search.get_domains()
                         all_hosts.extend(domains)
+                        
+                        print("[-] Searching in Cymon:")
+                        from discovery import cymon
+                        search = cymon.search_cymon(word)
+                        search.process()
+                        all_emails = []
+                        all_ip = search.get_ipaddresses()
+                        db = stash.stash_manager()
+                        db.store_all(word,all_ip,'ip','cymon')
 
-
-
+                        print("[-] Searching in Censys:")
+                        from discovery import censys
+                        search = censys.search_censys(word)
+                        search.process()
+                        all_emails = []
+                        all_ip = search.get_ipaddresses()
+                        all_hosts = search.get_hostnames()
+                        db = stash.stash_manager()
+                        db.store_all(word,all_ip,'ip','censys')
+                        db.store_all(word,all_hosts,'host','censys')
 
             else:
-            #if engine not in ("baidu", "bing", "crtsh","bingapi","dogpile","google", "googleCSE","virustotal","threatcrowd", "googleplus", "google-profiles","linkedin", "pgp", "twitter", "vhost", "yahoo","netcraft","all"):
                 usage()
-                print("Invalid search engine, try with: baidu, bing, bingapi, crtsh, dogpile, google, googleCSE, virustotal, netcraft, googleplus, google-profiles, linkedin, pgp, twitter, vhost, yahoo, hunter, all")
+                print("Invalid search engine, try with: baidu, bing, bingapi, crtsh, censys, cymon, dogpile, google, googleCSE, virustotal, netcraft, googleplus, google-profiles, linkedin, pgp, twitter, vhost, yahoo, hunter, all")
                 sys.exit()
-            #else:
-            #    pass
 
     #Results############################################################
     print("\n\033[1;32;40mHarvesting results")
