f5a8a195585c4305024cd516bb6784b6b70fe38d
Lee Baird <leebaird@gmail.com>
2018-12-18 21:39:57 -0600

    Whitespace

--- cymonparser.py
+++ cymonparser.py
@@ -6,7 +6,7 @@ class parser:
     def __init__(self, results):
         self.results = results
         self.ipaddresses = []
-        self.soup = BeautifulSoup(results.results,features="html.parser")
+        self.soup = BeautifulSoup(results.results, features="html.parser")
 
     def search_ipaddresses(self):
         try:
