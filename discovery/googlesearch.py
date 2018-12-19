990d84b8e55ee56e72052ab612c345978e769054
Laramies <laramies@gmail.com>
2018-12-19 22:36:31 +0100

    Update googlesearch.py

--- discovery/googlesearch.py
+++ discovery/googlesearch.py
@@ -14,17 +14,14 @@ class search_google:
         self.links = []
         self.database = "https://www.google.com/search?q="
         self.userAgent = ["(Mozilla/5.0 (Windows; U; Windows NT 6.0;en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6",
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-          ,("Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) " +
-          "AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36"),
-          ("Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152) " +
-          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254"),
-          "Mozilla/5.0 (SMART-TV; X11; Linux armv7l) AppleWebKit/537.42 (KHTML, like Gecko) Chromium/25.0.1349.2 Chrome/25.0.1349.2 Safari/537.42"
-          ,"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991"
-          ,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36 OPR/48.0.2685.52"
-          ,"Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"
-          ,"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
-          ,"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)"]
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+          "Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv)",
+          "AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36",
+          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15254",
+          "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36 OPR/48.0.2685.52",
+          "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"
+          ]
         self.quantity = "100"
         self.limit = limit
         self.counter = start
@@ -35,7 +32,7 @@ class search_google:
         except Exception as e:
             print(e)
         try:
-            params = {'User-Agent': random.choice(self.userAgent)} #select random user agent
+            params = {'User-Agent': self.userAgent[0]} #select random user agent
             r=requests.get(urly,params=params)
         except Exception as e:
             print(e)
@@ -76,7 +73,7 @@ class search_google:
             while self.counter <= self.limit and self.counter <= 1000:
                 self.do_search()
                 #more = self.check_next()
-                time.sleep(1)
+                time.sleep(1.5)
                 print("\tSearching " + str(self.counter) + " results...")
                 self.counter += 100
         else: #google dorking is true
@@ -93,7 +90,7 @@ class search_google:
     def process_profiles(self):
         while self.counter < self.limit:
             self.do_search_profiles()
-            time.sleep(0.15)
+            time.sleep(0.25)
             self.counter += 100
             print("\tSearching " + str(self.counter) + " results...")
 
