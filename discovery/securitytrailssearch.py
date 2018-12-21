562fcd7a34c83b7797ea9a63e39c16cd72af1ad9
NotoriousRebel <matt12299@yahoo.com>
2018-12-20 22:23:31 -0500

    Made search get subdomains and general information about domain, need to work on parsing results.

--- discovery/securitytrailssearch.py
+++ discovery/securitytrailssearch.py
@@ -1,8 +1,9 @@
 import myparser
 import requests
 import sys
+import time
 
-class search_hunter:
+class search_securitytrail:
 
     def __init__(self, word, limit, start):
         self.word = word
@@ -21,20 +22,32 @@ class search_hunter:
         #method to authenticate api key before sending requests
         headers = {'APIKEY': self.key}
         url = self.database + 'ping'
-        requests.get(url,headers=headers)
-
+        requests.get(url, headers=headers)
+        time.sleep(2)
 
     def do_search(self):
+        url = ''
         try:
-            r = requests.get(self.database)
+            #https://api.securitytrails.com/v1/domain/oracle.com?apikey=your_api_key
+            url = self.database + 'domain/' + self.word
+            headers = {'APIKEY': self.key}
+            r = requests.get(url, headers=headers)
+            time.sleep(2)
         except Exception as e:
             print(e)
         self.results = r.text
         self.totalresults += self.results
+        url += '/subdomains'
+        headers = {'APIKEY': self.key}
+        r = requests.get(url, headers=headers)
+        time.sleep(2)
+        self.results = r.text
+        self.totalresults += self.results
 
     def process(self):
-            self.do_search()
-            print('\tDone Searching Results')
+        self.authenticate()
+        self.do_search()
+        print('\tDone Searching Results')
 
     def get_emails(self):
         rawres = myparser.parser(self.totalresults, self.word)
