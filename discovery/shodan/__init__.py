6843b99a760b702bbab1a4acbb125a9a67c4e951
Janos Zold <janos.zold@hotmail.com>
2019-01-04 16:28:11 +0000

    Implement Shodan parser using the API

--- discovery/shodan/__init__.py
+++
@@ -1,3 +0,0 @@
-from discovery.shodan.api import WebAPI
-from discovery.shodan.client import Shodan
-from discovery.shodan.exception import APIError
--- discovery/shodansearch.py
+++ discovery/shodansearch.py
@@ -1,24 +1,43 @@
-from discovery.shodan import Shodan
+from shodan import Shodan
+from shodan import exception
+#from discovery.shodan import Shodan
 from discovery.constants import *
 
 
-class search_shodan():
+class search_shodan:
 
-    def __init__(self, host):
-        self.host = host
+    def __init__(self):
         self.key = shodanAPI_key
         if self.key == "":
             raise MissingKey(True)
         self.api = Shodan(self.key)
+        self.hostdatarow = []
 
-    def run(self):
+    def search_ip(self, ip):
         try:
-            result = self.api.host(self.host)
-            # for service in result['data']:
-            #    print ("%s:%s" % (service['ip_str'], service['port']))
-            #   print ("%s" % (service['product']))
-            #    print ("%s" % (service['hostnames']))
-            return result
+            ipaddress = ip
+            results = self.api.host(ipaddress)
+            technologies = []
+            servicesports = []
+            for result in results['data']:
+                try:
+                    for key in result['http']['components'].keys():
+                        technologies.append(key)
+                except KeyError as e:
+                    pass
+                port = str(result.get('port'))
+                product = str(result.get('product'))
+                servicesports.append(str(product)+':'+str(port))
+            technologies = list(set(technologies))
+            self.hostdatarow = [
+                    str(results.get('ip_str')), str(results.get('hostnames')).strip('[]\''),
+                    str(results.get('org')), str(servicesports).strip('[]\''),
+                    str(technologies).strip('[]\'')]
+        except exception.APIError:
+            print(ipaddress+": Not in Shodan")
+            self.hostdatarow = [ipaddress, "Not in Shodan", "Not in Shodan", "Not in Shodan", "Not in Shodan"]
+
         except Exception as e:
-            print("SHODAN empty reply or error in the call")
-            return "error"
+            print("Error occurred in the Shodan IP search module: " + str(e))
+        finally:
+            return self.hostdatarow
