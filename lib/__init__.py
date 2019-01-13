5016f3199e6465e48ae3f2c56863f00bbec5f139
Lee Baird <leebaird@gmail.com>
2019-01-13 00:17:06 -0600

    Replacing doulbe quotes with single quotes.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["markup", "graphs", "hostchecker"]
+__all__ = ['markup', 'graphs', 'hostchecker']
--- lib/htmlExport.py
+++ lib/htmlExport.py
@@ -5,7 +5,6 @@ import re
 
 class htmlExport():
 
-
     def __init__(self, users, hosts, vhosts, dnsres, dnsrev, file, domain, shodan, tldres):
         self.users = users
         self.hosts = hosts
@@ -86,9 +85,9 @@ class htmlExport():
         self.styler()
         page.head(self.style)
         page.body()
-        page.h1("theHarvester results")
-        page.h2("for :" + self.domain)
-        page.h3("Dashboard:")
+        page.h1('theHarvester results')
+        page.h2('for :' + self.domain)
+        page.h3('Dashboard:')
         graph = graphs.BarGraph('vBar')
         graph.values = [len(
             self.users),
@@ -99,56 +98,56 @@ class htmlExport():
         graph.labels = ['Emails', 'hosts', 'Vhost', 'TLD', 'Shodan']
         graph.showValues = 1
         page.body(graph.create())
-        page.h3("Emails found:")
+        page.h3('Emails found:')
         if self.users != []:
-            page.ul(class_="userslist")
-            page.li(self.users, class_="useritem")
+            page.ul(class_='userslist')
+            page.li(self.users, class_='useritem')
             page.ul.close()
         else:
-            page.h2("No emails found.")
-        page.h3("Hosts found:")
+            page.h2('No emails found.')
+        page.h3('Hosts found:')
         if self.hosts != []:
-            page.ul(class_="softlist")
-            page.li(self.hosts, class_="softitem")
+            page.ul(class_='softlist')
+            page.li(self.hosts, class_='softitem')
             page.ul.close()
         else:
-            page.h2("No hosts found.")
+            page.h2('No hosts found.')
         if self.tldres != []:
-            page.h3("TLD domains found in TLD expansion:")
-            page.ul(class_="tldlist")
-            page.li(self.tldres, class_="tlditem")
+            page.h3('TLD domains found in TLD expansion:')
+            page.ul(class_='tldlist')
+            page.li(self.tldres, class_='tlditem')
             page.ul.close()
         if self.dnsres != []:
-            page.h3("Hosts found in DNS brute force:")
-            page.ul(class_="dnslist")
-            page.li(self.dnsres, class_="dnsitem")
+            page.h3('Hosts found in DNS brute force:')
+            page.ul(class_='dnslist')
+            page.li(self.dnsres, class_='dnsitem')
             page.ul.close()
         if self.dnsrev != []:
-            page.h3("Hosts found with reverse lookup:")
-            page.ul(class_="dnsrevlist")
-            page.li(self.dnsrev, class_="dnsrevitem")
+            page.h3('Hosts found with reverse lookup:')
+            page.ul(class_='dnsrevlist')
+            page.li(self.dnsrev, class_='dnsrevitem')
             page.ul.close()
         if self.vhost != []:
-            page.h3("Virtual hosts found:")
-            page.ul(class_="pathslist")
-            page.li(self.vhost, class_="pathitem")
+            page.h3('Virtual hosts found:')
+            page.ul(class_='pathslist')
+            page.li(self.vhost, class_='pathitem')
             page.ul.close()
         if self.shodan != []:
             shodanalysis = []
-            page.h3("Shodan results:")
+            page.h3('Shodan results:')
             for x in self.shodan:
-                res = x.split("SAPO")
+                res = x.split('SAPO')
                 page.h3(res[0])
-                page.a("Port:" + res[2])
+                page.a('Port:' + res[2])
                 page.pre(res[1])
                 page.pre.close()
                 ban = res[1]
                 reg_server = re.compile('Server:.*')
                 temp = reg_server.findall(res[1])
                 if temp != []:
-                    shodanalysis.append(res[0] + ":" + temp[0])
+                    shodanalysis.append(res[0] + ':' + temp[0])
             if shodanalysis != []:
-                page.h3("Server technologies:")
+                page.h3('Server technologies:')
                 repeated = []
                 for x in shodanalysis:
                     if x not in repeated:
@@ -165,4 +164,4 @@ class htmlExport():
                 print('Exception' + x)  # Send to logs.
                 pass
         file.close()
-        return "ok"
+        return 'ok'
