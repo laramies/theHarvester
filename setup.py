44b67e97a8ca9e0fd99c6bb3f22664e3fab807df
fproldan <franciscoproldan@gmail.com>
2019-02-04 12:15:55 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='theHarvester',
+    version='3.0.6',
+    author="Christian Martorella",
+    author_email="cmartorella@edge-security.com",
+    description="theHarvester is a very simple, yet effective tool designed to be used in the early stages of a penetration test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/laramies/theHarvester",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+)
