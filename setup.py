e6e4e5621d6674e4c755c2d9c8b50261d44d455f
Anton Bolshakov <blshkv@users.noreply.github.com>
2019-03-24 10:15:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/laramies/theHarvester",
-    packages=setuptools.find_packages(),
+#    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
