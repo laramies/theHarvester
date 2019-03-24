70e42c3f91349f14f76152c52588c59fbed4cd2d
blshkv <blshkv@pentoo.ch>
2019-03-24 15:50:30 +0800

    rename the main script as it cannot be called the same as the module

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/laramies/theHarvester",
     packages=setuptools.find_packages(),
-    scripts=['theHarvester.py'],
+    scripts=['theharvester'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
