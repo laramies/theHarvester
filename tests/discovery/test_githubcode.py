26c151dd3f40d8e0520fc3128fee25f285b98758
Cameron Ruatta <c@ruatta.com>
2019-06-15 21:46:56 -0700

    Adding tests

---
+++ tests/discovery/test_githubcode.py
@@ -0,0 +1,44 @@
+from theHarvester.discovery import githubcode
+from theHarvester.discovery.constants import MissingKey
+from theHarvester.lib.core import Core
+from unittest.mock import MagicMock
+from requests import Response
+import pytest
+
+
+class TestSearchGithubCode:
+
+    def test_missing_key(self):
+        with pytest.raises(MissingKey):
+            githubcode.SearchGithubCode(word="test", limit=500)
+
+
+    def test_fragments_from_response(self):
+        Core.github_key = MagicMock(return_value="lol")
+        test_class_instance = githubcode.SearchGithubCode(word="test", limit=500)
+        response = Response()
+        json = {
+            "items": [
+                {
+                    "text_matches": [
+                        {
+                            "fragment": "test1"
+                        }
+                    ]
+                },
+                {
+                    "text_matches": [
+                        {
+                            "fragment": "test2"
+                        }
+                    ]
+                }
+            ]
+        }
+        response.json = MagicMock(return_value=json)
+        test_result = test_class_instance.fragments_from_response(response)
+        assert test_result == ["test1", "test2"]
+
+    if __name__ == '__main__':
+        pytest.main()
+
