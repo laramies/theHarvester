b47d68672662b6c2bf5e6af9be35f00eef729d7f
Matt <36310667+NotoriousRebel@users.noreply.github.com>
2019-08-21 16:58:20 -0400

    Update htmlExport.py

--- theHarvester/lib/htmlExport.py
+++ theHarvester/lib/htmlExport.py
@@ -160,8 +160,8 @@ class htmlExport():
         for x in page.content:
             try:
                 file.write(x)
-            except BaseException:
-                print('Exception' + x)  # Send to logs.
-                pass
+            except exception as e:
+		# Send to logs.
+                print(f'An exception has occurred in htmlexport: {e}')  
         file.close()
         return 'ok'
