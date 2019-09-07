a2a2e197ecab7f3d1829db1cdbc72801e96d39b7
csparks <csparksnj@gmail.com>
2019-09-07 03:35:59 +0000

    too many blank lines in test

--- tests/discovery/test_linkedin_links.py
+++ tests/discovery/test_linkedin_links.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # coding=utf-8
 from theHarvester.discovery import linkedinsearch
-from theHarvester.lib import stash
 import pytest
 
 
 class TestGetLinks(object):
-
 
     def test_get_links(self):
         search = linkedinsearch.SearchLinkedin("facebook.com", '100')
@@ -14,6 +12,7 @@ class TestGetLinks(object):
         links = search.get_links()
         for link in links:
             print(link)
+
 
 if __name__ == '__main__':
     pytest.main()
