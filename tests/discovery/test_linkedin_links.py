7ded50fd95d6cfef2ab4da066cb8eb917ac57041
csparks <csparksnj@gmail.com>
2019-09-07 03:00:22 +0000

    removed testing imports

--- tests/discovery/test_linkedin_links.py
+++ tests/discovery/test_linkedin_links.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # coding=utf-8
-import sys
-sys.path.append("../../")
 from theHarvester.parsers import myparser
 from theHarvester.discovery import linkedinsearch
-import pytest
 from theHarvester.lib import stash
+import pytest
 
 
 class TestGetLinks(object):
