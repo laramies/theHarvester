703e9e16a75c95e02d02d9e486eca7e666e24138
csparks <csparksnj@gmail.com>
2019-09-08 01:29:07 +0000

    closed the file after reading

--- tests/discovery/test_linkedin_links.py
+++ tests/discovery/test_linkedin_links.py
@@ -17,9 +17,11 @@ class TestGetLinks(object):
 
     def test_links_linkedin(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        mock_response = open(dir_path + "/test_linkedin_links.txt").read()
+        mock_response = open(dir_path + "/test_linkedin_links.txt")
+        mock_response_content = mock_response.read()
+        mock_response.close()
         reg_links = re.compile(r"url=https:\/\/www\.linkedin.com(.*?)&")
-        temp = reg_links.findall(mock_response)
+        temp = reg_links.findall(mock_response_content)
         resul = []
         for regex_item in temp:
             stripped_url = regex_item.replace("url=", "")
