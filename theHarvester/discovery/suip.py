515b28c7edfc14b72b13a2a9c3271a27a372e4c3
NotoriousRebel <matt12299@yahoo.com>
2019-10-02 13:36:53 -0400

    Switched from using subminder method in suip to using amass method in suip.

--- theHarvester/discovery/suip.py
+++ theHarvester/discovery/suip.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup
 
 class SearchSuip:
 
-    def __init__(self, word):
-        self.word = word
-        self.results = ''
-        self.totalresults = ''
-        self.totalhosts = set()
-        self.totalips = set()
+    def __init__(self, word: str):
+        self.word: str = word
+        self.results: str = ''
+        self.totalresults: str = ''
+        self.totalhosts: set = set()
+        self.totalips: set = set()
 
     def do_search(self):
-        base_url = "https://suip.biz/"
+        base_url: str = "https://suip.biz/"
         params = (
             ('act', 'subfinder'),
         )
@@ -22,25 +22,27 @@ class SearchSuip:
             'Submit1': 'Submit'
         }
 
-        headers = {'User-Agent': Core.get_user_agent()}
+        headers: dict = {'User-Agent': Core.get_user_agent()}
         try:
-            request = grequests.post(base_url, headers=headers, params=params, data=data)
-            data = grequests.map([request])
-            self.results = data[0].content.decode('UTF-8')
-            soup = BeautifulSoup(self.results, 'html.parser')
-            hosts = str(soup.find('pre')).splitlines()
-            self.clean_hosts(hosts)
-            params = (
-                ('act', 'amass'),
-            )
+            #request = grequests.post(base_url, headers=headers, params=params, data=data)
+            #data = grequests.map([request])
+            #self.results = data[0].content.decode('UTF-8')
+            #soup = BeautifulSoup(self.results, 'html.parser')
+            #hosts: list = str(soup.find('pre')).splitlines()
+            #self.clean_hosts(hosts)
+            pass
         except Exception as e:
             print(f'An exception has occurred: {e}')
         try:
+            params = (
+                ('act', 'amass'),
+            )
             request = grequests.post(base_url, headers=headers, params=params, data=data)
             data = grequests.map([request])
             self.results = data[0].content.decode('UTF-8')
             soup = BeautifulSoup(self.results, 'html.parser')
-            hosts = str(soup.find('pre')).splitlines()
+            print(soup.prettify())
+            hosts: list = str(soup.find('pre')).splitlines()
             self.clean_hosts(hosts)
         except Exception as e:
             print(f'An exception has occurred: {e}')
@@ -54,7 +56,6 @@ class SearchSuip:
 
     def clean_hosts(self, soup_hosts):
         for host in soup_hosts:
-            # print(type(host))
             host = str(host).strip()
             if len(host) > 1 and 'pre' not in host:
                 if host[0] == '.':
