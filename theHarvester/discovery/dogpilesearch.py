1b193ec446d8a7d354326a489d49e9b9fd27816c
NotoriousRebel <matt12299@yahoo.com>
2019-12-24 18:02:20 -0500

    Coverted dogpile from grequests to aiohttp.

--- theHarvester/discovery/dogpilesearch.py
+++ theHarvester/discovery/dogpilesearch.py
@@ -1,6 +1,5 @@
 from theHarvester.lib.core import *
 from theHarvester.parsers import myparser
-import grequests
 
 
 class SearchDogpile:
@@ -12,26 +11,25 @@ class SearchDogpile:
         self.hostname = 'www.dogpile.com'
         self.limit = limit
 
-    def do_search(self):
+    async def do_search(self):
         # Dogpile is hardcoded to return 10 results.
         try:
             headers = {'User-agent': Core.get_user_agent()}
             base_url = f'https://{self.server}/search/web?qsi=xx&q=%40{self.word}'
             urls = [base_url.replace("xx", str(num)) for num in range(0, self.limit, 10) if num <= self.limit]
-            req = (grequests.get(url, headers=headers, verify=False, timeout=5) for url in urls)
-            responses = grequests.imap(req, size=5)
+            responses = await async_fetcher.fetch_all(urls, headers=headers)
             for response in responses:
-                self.total_results += response.content.decode('UTF-8')
+                self.total_results += response
         except Exception as e:
             print(f'Error Occurred: {e}')
 
-    def process(self):
-        self.do_search()
+    async def process(self):
+        await self.do_search()
 
-    def get_emails(self):
+    async def get_emails(self):
         rawres = myparser.Parser(self.total_results, self.word)
         return rawres.emails()
 
-    def get_hostnames(self):
+    async def get_hostnames(self):
         rawres = myparser.Parser(self.total_results, self.word)
         return rawres.hostnames()
