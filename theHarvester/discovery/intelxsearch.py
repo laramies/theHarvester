989a062ba97e5c0eb7a01e8f162aa3e0a9190e95
NotoriousRebel <matt12299@yahoo.com>
2019-12-30 20:05:49 -0500

    Updated intelx search.

--- theHarvester/discovery/intelxsearch.py
+++ theHarvester/discovery/intelxsearch.py
@@ -1,7 +1,6 @@
 from theHarvester.discovery.constants import *
 from theHarvester.lib.core import *
 from theHarvester.parsers import intelxparser
-import requests
 import asyncio
 
 
@@ -24,15 +23,16 @@ class SearchIntelx:
             headers = {'User-Agent': user_agent, 'x-key': self.key}
             # data is json that corresponds to what we are searching for, sort:2 means sort by most relevant
             data = f'{{"term": "{self.word}", "maxresults": {self.limit}, "media": 0, "sort": 2 , "terminate": []}}'
-            r = await AsyncFetcher.post_fetch(url=f'{self.database}phonebook/search', headers=headers, data=data, json=True)
-            uuid = r['id']
+            resp = await AsyncFetcher.post_fetch(url=f'{self.database}phonebook/search', headers=headers, data=data,
+                                                 json=True)
+            uuid = resp['id']
             # grab uuid to send get request to fetch data
             await asyncio.sleep(2)
             url = f'{self.database}phonebook/search/result?id={uuid}&offset=0&limit={self.limit}'
-            r = await AsyncFetcher.fetch_all([url], headers=headers, json=True)
-            r = r[0]
+            resp = await AsyncFetcher.fetch_all([url], headers=headers, json=True)
+            resp = resp[0]
             # TODO: Check if more results can be gathered depending on status
-            self.results = r
+            self.results = resp
         except Exception as e:
             print(f'An exception has occurred: {e}')
 
