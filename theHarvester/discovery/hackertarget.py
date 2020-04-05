e3f656664facfc6a76a5b8acc1c2e8039f434449
NotoriousRebel <matt12299@yahoo.com>
2020-04-05 13:20:02 -0400

    Started to add hackertarget, TODO fix ips.

---
+++ theHarvester/discovery/hackertarget.py
@@ -0,0 +1,34 @@
+from theHarvester.lib.core import *
+
+
+class SearchHackerTarget:
+    """
+    Class uses the HackerTarget api to gather subdomains and ips
+    """
+
+    def __init__(self, word):
+        self.word = word
+        self.total_results = ""
+        self.hostname = 'https://api.hackertarget.com'
+        self.proxy = False
+        self.results = None
+
+    async def do_search(self):
+        headers = {'User-agent': Core.get_user_agent()}
+        urls = [f'{self.hostname}/hostsearch/?q={self.word}', f'{self.hostname}/reversedns/?q={self.word}']
+        responses = await AsyncFetcher.fetch_all(urls, headers=headers, proxy=self.proxy)
+        for response in responses:
+            self.total_results += response
+        self.results = list(zip(*[value.split(',') for value in self.total_results.splitlines()]))
+        print('Finished getting results')
+
+    async def process(self, proxy=False):
+        self.proxy = proxy
+        await self.do_search()
+
+    async def get_hostnames(self):
+        return self.results[0]
+
+    async def get_ips(self):
+        return []
+        #return self.results[1]
