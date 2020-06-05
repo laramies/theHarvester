6d9158f7cac33fbf4cfb221278c8782ff0d13f3b
L1ghtn1ng <townsend891@hotmail.com>
2020-06-05 15:48:26 +0100

    remove uneeded param and update deps for restapi

--- restfulHarvest.py
+++ restfulHarvest.py
@@ -6,9 +6,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-H', '--host', default='127.0.0.1', help='IP address to listen on default is 127.0.0.1')
 parser.add_argument('-p', '--port', default=5000, help='Port to bind the web server to, default is 5000')
-parser.add_argument('-l', '--log-level', default='info',
-                    help='Set logging level, default is info but [critical|error|warning|info|debug|trace] can be set')
-parser.add_argument('-r', '--reload', default=False, help='Enable auto-reload.', action='store_true')
+parser.add_argument('-l', '--log-level', default='info', help='Set logging level, default is info but [critical|error|warning|info|debug|trace] can be set')
 
 args = parser.parse_args()
 
