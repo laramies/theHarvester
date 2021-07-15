15de9dbe37053169977523280a779f275b5a6809
Jens Timmerman <github@caret.be>
2021-07-15 11:24:36 +0200

    Update hackertarget.py
    ignore the "no PTR records found" reply from hackertarget

--- theHarvester/discovery/hackertarget.py
+++ theHarvester/discovery/hackertarget.py
@@ -24,4 +24,4 @@ class SearchHackerTarget:
         await self.do_search()
 
     async def get_hostnames(self) -> list:
-        return self.total_results.splitlines()
+        return [x for x in self.total_results.splitlines() if not 'No PTR records found' in x]
