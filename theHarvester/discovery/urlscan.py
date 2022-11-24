1e2e658bf98fb6de7af3217cf456998fba4ab580
L1ghtn1ng <townsend891@hotmail.com>
2022-11-24 22:30:15 +0000

    fix static typing issues in urlscan.py

--- theHarvester/discovery/urlscan.py
+++ theHarvester/discovery/urlscan.py
@@ -1,14 +1,14 @@
-from typing import List
+from typing import List, Set
 from theHarvester.lib.core import *
 
 
 class SearchUrlscan:
     def __init__(self, word) -> None:
         self.word = word
-        self.totalhosts: List = list()
-        self.totalips: List = list()
-        self.interestingurls: List = list()
-        self.totalasns: List = list()
+        self.totalhosts: Set = set()
+        self.totalips: Set = set()
+        self.interestingurls: Set = set()
+        self.totalasns: Set = set()
         self.proxy = False
 
     async def do_search(self) -> None:
@@ -20,16 +20,16 @@ class SearchUrlscan:
         self.interestingurls = {f"{page['page']['url']}" for page in resp['results'] if self.word in page['page']['url'] and 'url' in page['page'].keys()}
         self.totalasns = {f"{page['page']['asn']}" for page in resp['results'] if 'asn' in page['page'].keys()}
 
-    async def get_hostnames(self) -> List:
+    async def get_hostnames(self) -> Set:
         return self.totalhosts
 
-    async def get_ips(self) -> List:
+    async def get_ips(self) -> Set:
         return self.totalips
 
-    async def get_interestingurls(self) -> List:
+    async def get_interestingurls(self) -> Set:
         return self.interestingurls
 
-    async def get_asns(self) -> List:
+    async def get_asns(self) -> Set:
         return self.totalasns
 
     async def process(self, proxy: bool = False) -> None:
