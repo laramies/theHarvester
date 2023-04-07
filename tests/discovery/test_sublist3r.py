6ddc66fc77b8d6d4fab231f5715aefab8940d970
NotoriousRebel <36310667+NotoriousRebel@users.noreply.github.com>
2023-04-07 16:04:24 -0400

    Removed broken modules.

--- tests/discovery/test_sublist3r.py
+++ tests/discovery/test_sublist3r.py
@@ -5,26 +5,23 @@ from theHarvester.lib.core import *
 from theHarvester.discovery import sublist3r
 import os
 import pytest
-from _pytest.mark.structures import MarkDecorator
-from typing import Optional
 
-pytestmark: MarkDecorator = pytest.mark.asyncio
-github_ci: Optional[str] = os.getenv('GITHUB_ACTIONS')  # Github set this to be the following: true instead of True
+pytestmark = pytest.mark.asyncio
+github_ci = os.getenv('GITHUB_ACTIONS')  # Github set this to be the following: true instead of True
 
 
 class TestSublist3r(object):
     @staticmethod
     def domain() -> str:
-        return 'google.com'
+        return 'target.com'
 
-    async def test_api(self) -> None:
+    async def test_api(self):
         base_url = f'https://api.sublist3r.com/search.php?domain={TestSublist3r.domain()}'
         headers = {'User-Agent': Core.get_user_agent()}
         request = requests.get(base_url, headers=headers)
         assert request.status_code == 200
 
-    @pytest.mark.skipif(github_ci == 'true', reason='Skipping on Github CI due unstable site')
-    async def test_do_search(self) -> None:
+    async def test_do_search(self):
         search = sublist3r.SearchSublist3r(TestSublist3r.domain())
         await search.process()
         assert isinstance(await search.get_hostnames(), list)
