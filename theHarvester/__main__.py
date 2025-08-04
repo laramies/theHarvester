872b40232ada19e1cd2aa792511ccd5bd21622e9
Lee Baird <leebaird@gmail.com>
2025-08-04 16:45:19 -0500

    Updated README, removed bing (#2081)
    * Removed bing.
    
    * Added API info
    
    * Update README.md
    
    * Removed shodan from modules section.
    
    * Fixed mistake
    
    * Update README.md
    
    * Removed comment.
    
    * Resolved conflict.
    
    * Added shodan

--- theHarvester/__main__.py
+++ theHarvester/__main__.py
@@ -18,7 +18,6 @@ from theHarvester.discovery import (
     api_endpoints,
     baidusearch,
     bevigil,
-    bingsearch,
     bravesearch,
     bufferoverun,
     builtwith,
@@ -165,7 +164,7 @@ async def start(rest_args: argparse.Namespace | None = None):
     parser.add_argument(
         '-b',
         '--source',
-        help="""baidu, bevigil, bing, bingapi, brave, bufferoverun,
+        help="""baidu, bevigil, brave, bufferoverun,
                             builtwith, censys, certspotter, criminalip, crtsh, dehashed, dnsdumpster, duckduckgo, fullhunt, github-code,
                             hackertarget, haveibeenpwned, hudsonrock, hunter, hunterhow, intelx, leaklookup, netlas, onyphe, otx, pentesttools,
                             projectdiscovery, rapiddns, rocketreach, securityscorecard, securityTrails, shodan, sitedossier, subdomaincenter,
@@ -415,30 +414,6 @@ async def start(rest_args: argparse.Namespace | None = None):
                         )
                     except Exception as e:
                         print(e)
-
-                elif engineitem == 'bing' or engineitem == 'bingapi':
-                    try:
-                        bing_search = bingsearch.SearchBing(word, limit, start)
-                        bingapi = ''
-                        if engineitem == 'bingapi':
-                            bingapi += 'yes'
-                        else:
-                            bingapi += 'no'
-                        stor_lst.append(
-                            store(
-                                bing_search,
-                                'bing',
-                                process_param=bingapi,
-                                store_host=True,
-                                store_emails=True,
-                            )
-                        )
-                    except Exception as e:
-                        if isinstance(e, MissingKey):
-                            if not args.quiet:
-                                print(e)
-                        else:
-                            print(e)
 
                 elif engineitem == 'brave':
                     try:
